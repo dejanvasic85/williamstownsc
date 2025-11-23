812a488ae50b784413eb31f1e570eac27615a794
Dejan Vasic <dejanvasic24@gmail.com>
2025-11-23 21:35:15 +1100

    Creating skeleton pages

---
+++ src/app/(site)/club/locations/page.tsx
@@ -0,0 +1,11 @@
+import { PageContainer } from '@/components/layout';
+
+export default function ClubLocationsPage() {
+	return (
+		<PageContainer>
+			<h1 className="border-secondary mb-8 border-b-4 pb-4 text-2xl font-bold lg:text-3xl">
+				Club Locations
+			</h1>
+		</PageContainer>
+	);
+}
---
+++ src/app/(site)/contact/page.tsx
@@ -0,0 +1,11 @@
+import { PageContainer } from '@/components/layout';
+
+export default function ContactPage() {
+	return (
+		<PageContainer>
+			<h1 className="border-secondary mb-8 border-b-4 pb-4 text-2xl font-bold lg:text-3xl">
+				Contact Us
+			</h1>
+		</PageContainer>
+	);
+}
