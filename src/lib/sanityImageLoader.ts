730067186c775cb5324b16c4bb35d38d61f0db89
Dejan Vasic <dejanvasic24@gmail.com>
2026-03-26 23:21:05 +1100

    Fixing hero image for desktop

--- src/lib/sanityImageLoader.ts
+++ src/lib/sanityImageLoader.ts
@@ -4,7 +4,17 @@ import type { ImageLoaderProps } from 'next/image';
 
 export function sanityImageLoader({ src, width, quality }: ImageLoaderProps): string {
 	const url = new URL(src);
+
+	const originalWidth = Number(url.searchParams.get('w'));
+	const originalHeight = Number(url.searchParams.get('h'));
+
 	url.searchParams.set('w', width.toString());
+
+	if (originalWidth && originalHeight) {
+		const scaledHeight = Math.round((width / originalWidth) * originalHeight);
+		url.searchParams.set('h', scaledHeight.toString());
+	}
+
 	url.searchParams.set('q', (quality || 75).toString());
 	url.searchParams.set('auto', 'format');
 	return url.toString();
